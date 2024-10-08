6fe27ffe609820195ef4a98e5f47c48ce0c1ba17
aditya38654 <aditya38654@gmail.com>
2024-10-08 15:25:04 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -4,6 +4,14 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+        x=dict(('()', '[]', '{}'))
+        stack=[]
+        for i in s:
+            if i in '({[':
+                stack.append(i)
+            elif not stack or x[stack.pop()]!=i:
+                return False
+        return not stack
         pass
 
 
