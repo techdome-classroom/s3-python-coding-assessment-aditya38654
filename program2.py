b4f258256cc146ca042674dade50eff9f2a2d487
aditya38654 <aditya38654@gmail.com>
2024-10-08 15:25:35 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -4,6 +4,14 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        dic={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        num=0
+        s=s.replace('IV','IIII').replace('IX','VIIII')
+        s=s.replace('XL','XXXX').replace('XC','LXXXX')
+        s=s.replace('CD','CCCC').replace('CM','DCCCC')
+        for i in s:
+            num+=dic[i]
+        return num
         pass
 
 
